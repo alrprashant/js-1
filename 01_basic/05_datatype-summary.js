4f7e7b6f522e203ac242349314b2345b0a94234c
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-23 12:49:04 +0000

    memmory done

--- 01_basic/05_datatype-summary.js
+++ 01_basic/05_datatype-summary.js
@@ -11,3 +11,36 @@
    type: array,object,function
 
 */
+
+
+
+
+
+
+
+
+
+
+// memories....................................
+//stack(premetive) and heap(non-permitive)
+
+// let myName="prashant";
+// let newName=myName;
+// newName="modi";
+
+
+
+// console.log(newName);
+// console.log(myName);
+
+
+
+let objectOne={
+   userName:"prashant",
+   address:"alrprashant009"
+}
+
+let objectTwo=objectOne;
+objectTwo.userName="alrPrashant";
+console.log(objectOne);
+
