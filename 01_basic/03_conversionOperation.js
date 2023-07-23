706c9952c87b24ae2373e2ab8cd942c512a6641d
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-23 10:42:07 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+// let score=33;
+let score="33";
+let valueInScore=Number(score);
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof(valueInScore));
+// console.log(valueInScore);
+
+// note: "33=>33"
+// "33a"=>"NaN"
+// true=>1
+// flase=0
+
+// let isLogedIn=1;
+// let booleanIsLogedIn=Boolean(isLogedIn);
+// console.log(booleanIsLogedIn);
+
+// 1=>true;
+//0=>false;
+// ""=>false;
+//"anyThing"=>true;
+
+let someNum=33;
+let stringNum=String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
+
