48d1da88008f56ce8e9148dafc8cd3fb7a3c5549
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-23 10:07:04 +0000

    variable done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const clientId=12345;
+let clientName ="kalu";
+var age=18;  //we will not going to use var due to scope problem 
+companyName="a13";
+// clientId=456;
+clientName="badaKalu";
+age=56;
+console.table([clientId,clientName,age,companyName]);
