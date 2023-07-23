e58a52e921ccb5c4d06fd9a8d2052087a82b8329
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-23 13:16:22 +0000

    half done

---
+++ 01_basic/06_string.js
@@ -0,0 +1,12 @@
+const userName="prashant";
+const userage=33;
+// console.log(userName+userage+"chalo");  ise ude nahi karna hai 
+
+console.log(`${userName} chalo${userage}`);
+console.log(typeof userName);
+
+
+
+//  actuiall way to write a string 
+const gameName=new String('zeero-katta');
+console.log(typeof gameName);
