e61bab3c3b951ca59e6d219f5cc65001100da493
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-25 06:35:18 +0000

    string okay

--- 01_basic/06_string.js
+++ 01_basic/06_string.js
@@ -2,11 +2,64 @@ const userName="prashant";
 const userage=33;
 // console.log(userName+userage+"chalo");  ise ude nahi karna hai 
 
-console.log(`${userName} chalo${userage}`);
-console.log(typeof userName);
+// console.log(`${userName} chalo${userage}`);
+// console.log(typeof userName);
 
 
 
 //  actuiall way to write a string 
 const gameName=new String('zeero-katta');
-console.log(typeof gameName);
+// console.log(typeof gameName);
+// console.log(gameName.length);
+// console.log(gameName.indexOf('t'));  
+
+// substring  && slice
+// const newString=gameName.substring(0,4);
+// console.log(newString);
+// const newString1=gameName.slice(0,4);
+// console.log(newString1);
+// const newString2=gameName.substring(4,0);
+// console.log(newString2);
+// const newString3=gameName.substring(-4,4);
+// console.log(newString3);
+// const newString4=gameName.slice(-4,8);
+// console.log(newString4);
+
+// trim 
+
+// const string312=" alr  ";
+// console.log(string312);
+// console.log(string312.trim());
+
+// const string3121="     alr prashant  ";
+// console.log(string3121);
+// console.log(string3121.trim());
+
+//  replace 
+
+const url="alrprashant009@gmail.com";
+// console.log(url.replace('009','-'));
+
+// includes
+
+console.log(url.includes('alr'));
+console.log(url.includes('modi'));
+
+//  split used to make an array by string 
+const str344="alr-prashant-009";
+console.log(str344.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
