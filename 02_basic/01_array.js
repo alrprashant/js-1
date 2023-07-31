7164cc05e6c54b7d7048175f4d54872e54d2a242
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-07-31 16:27:33 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,69 @@
+// array
+const myArr=[0,1,2,3,4,5];
+const myHeros=["shaktiman" ,"nagraj"];
+const myArr2=new Array(1,2,3,4,5);
+
+// console.log(myArr2);
+// console.log(myArr[3]);
+
+// array methods
+// 1 push in an array
+myArr.push(6);
+// console.log(myArr);
+myArr.push(7);
+// console.log(myArr);
+
+// 2 pop 
+myArr.pop();
+// console.log(myArr);
+myArr.pop();
+// console.log(myArr);
+myArr.pop();
+// console.log(myArr);
+//3  unshift 
+myArr.unshift(7);
+// console.log(myArr);
+myArr.unshift(8);
+// console.log(myArr);
+// 4 shift
+myArr.shift();
+// console.log(myArr);
+// 5 includes 
+// console.log(myArr.includes(33));
+
+// 6 index off 
+// console.log(myArr.indexOf(4));
+// console.log(myArr.indexOf(14));
+
+
+// console.log(typeof myArr);
+// // join 
+// const newArr=myArr.join();
+// console.log(typeof newArr);
+// console.log(newArr);
+
+
+// console.log("a",myArr);
+
+const myArr3=[1,2,3,4,5];
+const myn1=myArr3.slice(1,3);
+console.log(myn1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
