5b1a99e67a0859ef0b68bab6091ac37f76f8f248
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-08-22 22:00:56 +0000

    function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,16 @@
+const user={
+    name:"alr",
+    Price:199
+}
+
+function handleobject(anyobject){
+    console.log(`${anyobject.name} ka price ${anyobject
+    .Price} hai !`)
+
+}
+
+handleobject(user);
+// handleobject({
+//     name:"alr",
+//     Price:199
+// });
