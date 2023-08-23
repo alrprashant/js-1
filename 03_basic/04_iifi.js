dbc08ff9674f62405b8202feb720e41999e824bf
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-08-23 20:02:42 +0000

    iifi done

---
+++ 03_basic/04_iifi.js
@@ -0,0 +1,16 @@
+// immediately invoked function impressioon 
+
+
+// ()(); ** it"s simple expresion of an iife 
+
+(
+    function chai(){
+        console.log("chai pee lo");
+    }
+)();
+
+
+((name)=>{
+    console.log(`${name}`);
+
+})("alr prashant");
