e7a1dd4f420188d832a7b5e5dd8cffc34b70911c
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-08-23 18:31:29 +0000

    scope done

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,11 @@
+function one(){
+    const username="alr prashant ";
+    function two(){
+        const  website="youtube";
+        console.log(username);
+    }
+    
+    two();
+    // console.log(website);
+}
+one();
