8955d9edd96c040c1ebdc717fd8567f8a63089ed
Alr Prashant <102617781+alrprashant@users.noreply.github.com>
2023-08-25 19:20:41 +0000

    forn in ;

--- 04_iteration.js/four.js
+++ 04_iteration.js/four.js
@@ -1,10 +1,16 @@
-const object1={
-    js:"javascript",
-    cpp:"c++",
-    swift:"swift by apple"
-}
+// const object1={
+//     js:"javascript",
+//     cpp:"c++",
+//     swift:"swift by apple"
+// }
 
 
-for (const key in object1) {
-   console.log(key);
+// for (const key in object1) {
+//    console.log(key);
+// }
+
+const arr=[1,2,3,4,5];
+for (const key  in arr) {
+   console.log(arr[key]);
 }
+// in for in array"s properties can be acessable by given way
